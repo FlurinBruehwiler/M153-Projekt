d71487324047e8dd7ccefb758776bf447c1980bc
kochabelgisi <47397416+FlurinBruehwiler@users.noreply.github.com>
2021-12-10 22:27:50 +0100

    api update

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,13 @@ const bodyParser = require("body-parser");
 const mysql = require('mysql2');
 const port = 5000;
 
-/*
+
 const db = mysql.createConnection({
     host: 'localhost',
     user : 'root',
     password : 'secret',
-    database: 'nodejstest'
+    database: 'filmplattform'
 });
-*/
-
-const sql = "select * from filme";
 
 app.use(express.json());
 
@@ -22,20 +19,27 @@ app.use(bodyParser.json());
 
 app.listen(port, ()=> console.log("App is running very fast"));
 
-/*
+
+
 app.get('/filme', (req, res) => {
-    db.query(
-        sql,
-        function(err, results, fields) {
+    const sql = "select * from film";
+    db.query(sql, (err, results) => {
+            if(err) throw err;
+            console.log(results);
             res.send(results);
         }
     );
 });
-*/
+
 
 app.post('/newFilm', (req, res) => {
-    console.log(req.body);
-    res.status(200);
-    res.send();
+    let shortDescription = req.body.shortDescription === '' ? 'null' : `'${req.body.shortDescription}'`;
+
+    const sql = `INSERT INTO film (Film_Id, Film_Title, Film_ReleaseDate, Film_ShortDescription, Film_LongDescription) 
+    VALUES (NULL, '${req.body.title}', '${req.body.releaseDate}', ${shortDescription}, '${req.body.longDescription}')`;
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.status(200).send();
+    })
 })
 
